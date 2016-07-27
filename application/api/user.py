27fbe2ba625efe55bb49f526390e4d6dc2d3c13c
taewookimfardark <2012142087@yonsei.ac.kr>
2016-07-28 00:23:16 +0900

    user

--- application/api/user.py
+++ application/api/user.py
@@ -2,6 +2,36 @@ from . import api
 from application import db
 from application.models.user import User
 
+from flask import request, jsonify
+from application.lib.rest.rest_query_helper import (
+    model_to_dict
+)
+from application.lib.rest.auth_helper import (
+    get_user_data_from_request,
+    required_token
+)
+
+from application.lib.storage.cloud_storage_helper import upload_image
+
+import json
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+
 @api.route("/")
 def heool():
     return "hello woohwa"
+
+@api.route("/users", methods=['GET'])
+def get_users():
+    users = User.query.all()
+    query_list = []
+    for user in users:
+        temp = model_to_dict(user)
+        query_list.append(temp)
+
+    return jsonify(
+        query_list
+    )
+
--- application/models/user.py
+++ application/models/user.py
@@ -5,10 +5,12 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(200))
     password = db.Column(db.String(500))
+    photourl = db.Column(db.String(2000))
     def get_token_string(self):
         data = {
             "email" : self.email,
             "id" : self.id
         }
         return jwt_encode(data)
+
 
